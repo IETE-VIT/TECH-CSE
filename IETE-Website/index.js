df2cc2368f2c3034295db1ed8ed5e92869364b40
Nitish-JS <73771450+Nitish-JS@users.noreply.github.com>
2021-10-02 16:55:45 +0530

    Add files via upload

---
+++ IETE-Website/index.js
@@ -0,0 +1,14 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    margin:30,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        1000:{
+            items:3
+        }
+    }
+})
